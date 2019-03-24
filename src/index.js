3b53fe321a372add996d3f4f021d94aedbc79094
merlox <merunasgrincalaitis@gmail.com>
2019-03-24 10:53:07 +0100

    Setup initial webpack'

--- src/index.js
+++ src/index.js
@@ -1,14 +1,43 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import './index.css'
 
 class Main extends React.Component {
     constructor() {
         super()
+
+        this.state = {
+            content: [{
+                author: '0x211824098yf7320417812j1002341342342341234',
+                message: 'This is a test',
+                hashtags: ['test', 'dapp', 'blockchain']
+            }, {
+                author: '0x211824098yf7320417812j1002341342342341234',
+                message: 'This is another test',
+                hashtags: ['sample', 'dapp', 'Ethereum']
+            }]
+        }
     }
 
     render() {
         return (
-            <div>The project has been setup. Remember to npm install and webpack -w -d to see your changes.</div>
+            <div>
+                <div className="hashtag-block">
+                    <h2>Most popular hashtags</h2>
+                    <div className="hashtag-container"></div>
+                </div>
+                <div className="content-block">
+                    <div className="input-container">
+                        <input placeholder="Publish content..." type="text"/>
+                        <button type="button">Publish</button>
+                        <button type="button">Cancel</button>
+                    </div>
+
+                    <div className="content-container">
+
+                    </div>
+                </div>
+            </div>
         )
     }
 }
